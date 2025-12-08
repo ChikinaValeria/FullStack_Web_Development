d36266ac0598bfa0ca1de1cc9d68b542ba1fae3e
Valeria Chikina <dliavajnogo@gmail.com>
2025-12-08 08:37:41 +0200

    Correct server.js

--- MovieCollectionAppDBAuth/server.js
+++ MovieCollectionAppDBAuth/server.js
@@ -3,38 +3,26 @@ import dotenv from "dotenv";
 dotenv.config();
 
 import express from "express";
-import { MongoClient } from "mongodb";
+import movieRouter from "./routes/movie.js";
+import { initDatabase } from "./config/dbConfig.js";
+//import authenticationRouter from "./routes/authentication.js"
+
+const port = process.env.PORT;
+//init the database
+await initDatabase()
 
 const app = express()
 app.use(express.json())
+app.use('/movies', movieRouter )
+app.use((req, res) => {
+  res.status(404).send("Oops. Not found!");
+});
 
-// Read the environment values from .env
-const uri = process.env.MONGODB_URI;
-if(!uri){
-    console.error("Problem with uri");
-    process.exit(1);
-}
-const dbName = process.env.MONGODB_NAME;
-const port = process.env.PORT;
-
-// Connect the MongoDB Cluster
-const client = new MongoClient(uri)
-
-
-
-//init the database
-await initDatabase()
 
 //start the server
 app.listen( port, () => {
     console.log("Server listening on http://localhost:3000")
 })
 
-// DB connection is over the cloud so every operation is async
-
-// 404 fallback
-app.use((req, res) => {
-  res.status(404).send("Oops. Not found!");
-});
 
 
